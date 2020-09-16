b9813458c884bf48ce2addebaa487bccee50f83a
Gbi92 <70910864+Gbi92@users.noreply.github.com>
2020-09-16 20:17:14 +0200

    added autofocus and reaction to enter button and tick-untick action

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 let count = 3;
 
 $('#mainButton').click(() => {
+    if($('input').val().trim() !== ''){
     $('ul').append(`<li id="todo-${count}">${$('input').val()}</li>`);
     $('.icons-holder').append(`<div class="icons" id="icon-${count}">
     <img src="trashBin.svg" id="dlt-${count}" class="delete-button">
     <img src="tick.svg" id="tick-${count}" class="complete-button">
 </div> `)
     count++;
+    $('input').val('');
+    }
   });
 
+$('input').keypress(event => {
+  if(event.which == 13) {
+    $('#mainButton').trigger('click');
+  }
+})  
+
 $('.todos-container').on('click', '.delete-button', (event) => {
-    console.log(event.target);
     let currentIndex = event.target.id.split("-")[1];
     $(`#todo-${currentIndex}`).remove();
     $(`#icon-${currentIndex}`).remove();
   });
 
   $('.todos-container').on('click', '.complete-button', (event) => {
-    console.log(event.target);
     let currentIndex = event.target.id.split("-")[1];
-    $(`#todo-${currentIndex}`).css('text-decoration', 'line-through');
-    $(`#tick-${currentIndex}`).css('background', 'salmon',);
+    if ($(`#todo-${currentIndex}`).css('text-decoration').indexOf('line-through') != -1) {
+      $(`#todo-${currentIndex}`).css('text-decoration', 'none');
+      $(`#tick-${currentIndex}`).css('background', 'transparent',);
+    }else {
+      $(`#todo-${currentIndex}`).css('text-decoration', 'line-through');
+      $(`#tick-${currentIndex}`).css('background', 'salmon',);
+    }
   });
